9cd2553d00bc2667d9b257d552dba95ada59eb60
DenDen047 <sh.mn.nat@gmail.com>
2019-01-30 17:48:49 +0900

    portを変更

--- tensorboard.sh
+++ tensorboard.sh
@@ -5,6 +5,6 @@ CMD="tensorboard --logdir=$1 --port=6006"
 docker run -it --rm \
     -v /data2/naoya/pybullet_envs:/data \
     -w /data \
-    -p 6006:6006 \
+    -p 6007:6006 \
     tensorflow/tensorflow \
     /bin/bash -c "${CMD}"
